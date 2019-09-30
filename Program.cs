3fc9ba5042daadebde46034ceca46e84a5ca4ed3
Mikael <tm.mikaeljonsson@gmail.com>
2019-09-30 08:55:18 +0200

    Add initial menu and exit functionalty

--- Program.cs
+++ Program.cs
@@ -7,7 +7,37 @@ namespace Restaurant
     {
         static void Main(string[] args)
         {
-            
+
+            bool shouldRun = true;
+
+            while (shouldRun)
+            {
+                Clear();
+
+                WriteLine("1. Add order");
+                WriteLine("2. List orders");
+                WriteLine("3.Exit");
+
+                ConsoleKeyInfo keyPressed = ReadKey();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        shouldRun = false;
+
+                        break;
+
+
+                }
+            }
         }
     }
 }
